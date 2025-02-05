ce13220dde85389f8ea15caf208b9d0b72c16403
Jonathan Garcia <jonathan.ray.garcia@gmail.com>
2025-02-04 19:34:47 -0800

    fix: update login data flow to include local cached data

--- src/context/UserLogin/UserLoginProvider.tsx
+++ src/context/UserLogin/UserLoginProvider.tsx
@@ -7,29 +7,54 @@ import { googleLoginInit, openGoogleLoginPrompt } from './../../vendor/google/go
 import { FetcherResponse } from '../../utils/FetcherTypes';
 import { toLoginRequestBody } from './utils'
 
+enum LOGIN_STATES {
+  INITIALIZE,
+  LOCAL_CHECKED,
+  GOOGLE_SIGNED_IN,
+  LOGGED_IN,
+  LOGGED_OUT
+}
+const expirationDate = () => new Date(
+  Date.now() + (7 * (24 * 60 * 60 * 1000))
+).toDateString()
+
 function UserLoginProvider({ children }: { children: ReactNode}) {
   
   const fetcher = useContext(FetcherContext)
+  const [loginState, setLoginState] = useState<LOGIN_STATES>(LOGIN_STATES.INITIALIZE)
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [googleLoginData, setGoogleLoginData] = useState<unknown | null>(null)
   const [userLoginData, setUserLoginData] = useState<User | null>(null);
-  const [localUserId] = useState(sessionStorage.getItem('tasker::userId'))
-  const [localUserData] = useState<string | null>(JSON.parse(sessionStorage.getItem('tasker::userData') || 'null'))
+  const [localUserId] = useState<{res: boolean, expiry: string} | null>(JSON.parse(sessionStorage.getItem('tasker::userId') || 'null'))
+  const [localUserData] = useState<{res: User, expiry: string} | null>(JSON.parse(sessionStorage.getItem('tasker::userData') || 'null'))
 
   useEffect(() => {
-    googleLoginInit().then((googleLoginData) => {
-      setGoogleLoginData(googleLoginData)
-    })
-  }, [localUserId])
+    if ([LOGIN_STATES.INITIALIZE, LOGIN_STATES.LOGGED_OUT].includes(loginState)) {
+      const isIdValid = localUserId?.expiry && new Date(localUserId?.expiry) > new Date()
+      const isUserDataValid = localUserData?.expiry && new Date(localUserData?.expiry) > new Date()
+
+      if (isIdValid && isUserDataValid) {        
+          setUserLoginData(localUserData.res)
+          setLoginState(LOGIN_STATES.LOGGED_IN);
+      } else {
+        setLoginState(LOGIN_STATES.LOCAL_CHECKED)
+      }
+    }
+  }, [loginState, localUserData, localUserId])
+  
 
   useEffect(() => {
-    if (!localUserId) openGoogleLoginPrompt()
-   }, [localUserId]);
+    if (loginState === LOGIN_STATES.LOCAL_CHECKED) {
+      googleLoginInit().then((googleLoginData) => {
+        setGoogleLoginData(googleLoginData)
+        setLoginState(LOGIN_STATES.GOOGLE_SIGNED_IN)
+      })
+      openGoogleLoginPrompt()
+    }
+  }, [loginState])
 
-   useEffect(() => {
-
-    if (localUserId || googleLoginData) {
-
+  useEffect(() => {
+    if (loginState === LOGIN_STATES.GOOGLE_SIGNED_IN) {
       const requestBody = toLoginRequestBody({localUserId, googleLoginData})
 
       fetcher.post<User>({
@@ -37,19 +62,33 @@ function UserLoginProvider({ children }: { children: ReactNode}) {
         body: requestBody
       }).then((data: FetcherResponse<User>)=> {
         const {err, res} = data
+        const expiry = expirationDate()
         if (err || !res) {
-          //handle error
+          setLoginState(LOGIN_STATES.LOGGED_OUT)
         } else {
           setUserLoginData(res)
-          setIsLoggedIn(true)
-          sessionStorage.setItem('tasker::userId', res.userId)
-          sessionStorage.setItem('tasker::usreData', JSON.stringify(res))
+          
+          sessionStorage.setItem(
+            'tasker::userId', 
+            JSON.stringify({
+              value: res.userId, 
+              expiry
+            })
+          )
+          sessionStorage.setItem(
+            'tasker::userData', 
+            JSON.stringify({res, expiry})
+          )
+          setLoginState(LOGIN_STATES.LOGGED_IN)
         }
       });
     }
-
-   }, [localUserId, googleLoginData, fetcher])
+  }, [loginState, localUserId, googleLoginData, fetcher])
    
+  useEffect(() => {
+    if (loginState === LOGIN_STATES.LOGGED_IN) setIsLoggedIn(true)
+  }, [loginState])
+
   return (
     <UserLoginContext.Provider value={{
       isLoggedIn, 
