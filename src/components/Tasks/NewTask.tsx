257e08f6bd8c5f04b77fb068f61c8c205eb3ea85
Jonathan Garcia <jonathan.ray.garcia@gmail.com>
2025-02-11 07:17:20 -0800

    fix: remove debug code

--- src/components/Tasks/NewTask.tsx
+++ src/components/Tasks/NewTask.tsx
@@ -45,7 +45,7 @@ const NewTask = () => {
                 onChange={onDescChange}
                 className="border-1 border-white/10 bg-slate-800 rounded-lg text-med/2 text-white/90 p-4 mt-6 focus:outline-1 focus-outline-offsest-1"
               ></textarea>
-                <h2>HEY:{`${isPublic}`}</h2>
+
                 <button
                   className="items-center gap-2 flex flex-row hover:cursor-pointer my-4 outline-1 hover:outline-2 outline-white/20 w-min px-6 py-2 rounded-lg outline-offset-1"
                   onClick={(event) => onIsPublicClick(event, !!isPublic)}>
