ba96286fec87ddc36061e03908a937ceca1412a0
Jonathan Garcia <jonathan.ray.garcia@gmail.com>
2025-02-22 14:54:44 -0800

    fix: removing unused old context

--- src/utils/FetcherContext.tsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-import { ENDPOINTS } from '../constants/endpoints';
-import { Fetcher } from '../utils/fetcher';
-
-const fetcher = new Fetcher({ BASE_URL: ENDPOINTS.BASE_URL})
-export const FetcherContext = createContext(fetcher);
